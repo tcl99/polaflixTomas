44a54bcc16cb0cec78205d8459b344c5b376d279
Tomás <tcl218@alumnos.unican.es>
2023-04-07 20:16:57 +0200

    Personalización con nombre OK

--- src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/SerieRepository.java
+++ src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/SerieRepository.java
@@ -3,13 +3,10 @@ package es.unican.alumnos.tcl218.polaflix_tom.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import es.unican.alumnos.tcl218.polaflix_tom.DomainModel.Visualizacion.Serie;
 
-@Repository
 public interface SerieRepository extends JpaRepository<Serie, Long> {
-
     List<Serie> findByInfoTituloOrderByInfoTitulo(String titulo);
     List<Serie> findByInfoGenero(String genero);
     List<Serie> findByInfoCategoria(String categoria);
--- src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/UsuarioRepository.java
+++ src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/UsuarioRepository.java
@@ -1,11 +1,9 @@
 package es.unican.alumnos.tcl218.polaflix_tom.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import es.unican.alumnos.tcl218.polaflix_tom.DomainModel.Usuario.Usuario;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
     
 }
