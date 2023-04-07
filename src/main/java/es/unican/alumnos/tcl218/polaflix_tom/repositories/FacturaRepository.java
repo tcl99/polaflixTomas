dd01cc06964daed175f0136623a2784837034cf7
Tomás <tcl218@alumnos.unican.es>
2023-04-07 16:28:32 +0200

    Nombres de metodo OK

--- src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/FacturaRepository.java
+++ src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/FacturaRepository.java
@@ -1,6 +1,7 @@
 package es.unican.alumnos.tcl218.polaflix_tom.repositories;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Repository;
 import es.unican.alumnos.tcl218.polaflix_tom.DomainModel.Cobros.Factura;
 
 @Repository
-public interface FacturaRepository extends JpaRepository<Factura, Date> {
-    
+public interface FacturaRepository extends JpaRepository<Factura, Long> {
+    List<Factura> findByFechaOrderByImporteMensual(Date d);
+    List<Factura> findByFechaBeforeOrderByFecha(Date d);
+    List<Factura> findByFechaAfterOrderByFecha(Date d);
+    List<Factura> findByImportesNombreSerie(String titulo);
 }
--- src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/SerieRepository.java
+++ src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/SerieRepository.java
@@ -1,5 +1,7 @@
 package es.unican.alumnos.tcl218.polaflix_tom.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import es.unican.alumnos.tcl218.polaflix_tom.DomainModel.Visualizacion.Serie;
 
 @Repository
 public interface SerieRepository extends JpaRepository<Serie, Long> {
-    
+
+    List<Serie> findByInfoTituloOrderByInfoTitulo(String titulo);
+    List<Serie> findByInfoGenero(String genero);
+    List<Serie> findByInfoCategoria(String categoria);
 }
