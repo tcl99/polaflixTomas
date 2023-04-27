181020e3b4d431b27e877a0fefad595818d11660
tclD <tcl218@alumnos.unican.es>
2023-04-27 14:35:47 +0200

    @Repository

--- src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/FacturaRepository.java
+++ src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/FacturaRepository.java
@@ -4,9 +4,11 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.unican.alumnos.tcl218.polaflix_tom.DomainModel.Cobros.Factura;
 
+@Repository
 public interface FacturaRepository extends JpaRepository<Factura, Long> {
     List<Factura> findByFechaOrderByImporteMensual(Date d);
     List<Factura> findByFechaBeforeOrderByFecha(Date d);
--- src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/SerieRepository.java
+++ src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/SerieRepository.java
@@ -3,11 +3,13 @@ package es.unican.alumnos.tcl218.polaflix_tom.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.unican.alumnos.tcl218.polaflix_tom.DomainModel.Visualizacion.Serie;
 
+@Repository
 public interface SerieRepository extends JpaRepository<Serie, Long> {
-    List<Serie> findByInfoTituloOrderByInfoTitulo(String titulo);
+    Serie findByInfoTitulo(String titulo);
     List<Serie> findByInfoGenero(String genero);
     List<Serie> findByInfoCategoria(String categoria);
 }
