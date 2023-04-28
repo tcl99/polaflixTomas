0c1f9a8324b19bc7bd08fa9d340719ab8e27b565
tclD <tcl218@alumnos.unican.es>
2023-04-28 14:33:00 +0200

    @Query para buscar series por inicial

--- src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/SerieRepository.java
+++ src/main/java/es/unican/alumnos/tcl218/polaflix_tom/repositories/SerieRepository.java
@@ -3,8 +3,11 @@ package es.unican.alumnos.tcl218.polaflix_tom.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import es.unican.alumnos.tcl218.polaflix_tom.DomainModel.Visualizacion.InformacionSerie;
 import es.unican.alumnos.tcl218.polaflix_tom.DomainModel.Visualizacion.Serie;
 
 @Repository
@@ -12,4 +15,10 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     Serie findByInfoTitulo(String titulo);
     List<Serie> findByInfoGenero(String genero);
     List<Serie> findByInfoCategoria(String categoria);
+
+    @Query("SELECT info FROM Serie WHERE SUBSTRING(info.titulo, 1, 1) = :inicial ORDER BY info.titulo")
+    List<InformacionSerie> findSerieByInicialLetra(@Param("inicial")String inicial);
+
+    @Query("SELECT info FROM Serie WHERE SUBSTRING(info.titulo, 1, 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') ORDER BY info.titulo ASC ")
+    List<InformacionSerie> findSerieByInicialNumero();
 }
